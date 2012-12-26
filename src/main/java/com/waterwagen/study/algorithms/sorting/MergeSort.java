5e5969d571089a3207a844b8f118f961c2048e08
Josh Taylor <waterwagen@gmail.com>
2012-12-26 18:46:27 +0000

    A small optimization of the MergeSort implementation.

--- src/main/java/com/waterwagen/study/algorithms/sorting/MergeSort.java
+++ src/main/java/com/waterwagen/study/algorithms/sorting/MergeSort.java
@@ -38,17 +38,18 @@ public class MergeSort<T extends Comparable<T>> implements Sort<T>
 		for(int index = 0; index < helper.length; index++)
 			helper[index] = list.get(low1 + index);
 		
+		int helper_high1 = low2 - low1 - 1;
 		int merge_pointer = low1;
 		int pointer_one = 0;
-		int pointer_two = low2 - low1;
-		while(pointer_one < (low2 - low1) && pointer_two < helper.length)
+		int pointer_two = helper_high1 + 1;
+		while(pointer_one <= helper_high1 && pointer_two < helper.length)
 		{
 			int compare_result = helper[pointer_one].compareTo(helper[pointer_two]);
 			int lowest_value_index = compare_result <= 0 ? pointer_one++ : pointer_two++;
 			list.set(merge_pointer, helper[lowest_value_index]);
 			merge_pointer++;
 		}
-		while(pointer_one < (low2 - low1))
+		while(pointer_one <= helper_high1)
 			list.set(merge_pointer++, helper[pointer_one++]);
 	}
 }
