4a8e8da1dc268ba20469f2ebae0c02700bfa0868
Josh Taylor <waterwagen@gmail.com>
2012-12-26 23:12:53 +0000

    Created an AlgorithmExecutor for the last method/message type on UnionQuickFind.

--- src/main/java/com/waterwagen/algorithms/evaluate/DoublingRatioRunner.java
+++ src/main/java/com/waterwagen/algorithms/evaluate/DoublingRatioRunner.java
@@ -1,18 +1,19 @@
 package com.waterwagen.algorithms.evaluate;
 
-import com.waterwagen.algorithms.evaluate.implementations.sorts.QuickSortExecutor;
+import com.waterwagen.algorithms.evaluate.implementations.unionfind.UnionQuickFindExecutorForComponentCountMsg;
 
 public class DoublingRatioRunner
 {
 	public static void main(String[] argv)
 	{
-		AlgorithmExecutor executor = new QuickSortExecutor();
+//		AlgorithmExecutor executor = new QuickSortExecutor();
 //		AlgorithmExecutor executor = new MergeSortExecutor();
 //		AlgorithmExecutor executor = new BubbleSortExecutor();
 //		AlgorithmExecutor executor = new BinarySearchExecutor();
 //		AlgorithmExecutor executor = new UnionQuickFindExecutorForFindMsg();
 //		AlgorithmExecutor executor = new UnionQuickFindExecutorForConnectedMsg();
 //		AlgorithmExecutor executor = new UnionQuickFindExecutorForUnionMsg();
+		AlgorithmExecutor executor = new UnionQuickFindExecutorForComponentCountMsg();
 		DoublingRatioFinder.run(executor);
 	}
 }
---
+++ src/main/java/com/waterwagen/algorithms/evaluate/implementations/unionfind/UnionQuickFindExecutorForComponentCountMsg.java
@@ -0,0 +1,11 @@
+package com.waterwagen.algorithms.evaluate.implementations.unionfind;
+
+
+public class UnionQuickFindExecutorForComponentCountMsg extends AbstractUnionQuickFindExecutor
+{
+	@Override
+	public void execute()
+	{
+		mUnionQuickFind.componentCount();
+	}
+}
