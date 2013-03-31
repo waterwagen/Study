091f7b5236a94d622241d593cb9dd3672e721291
Josh Taylor <waterwagen@gmail.com>
2013-03-31 20:33:16 +0000

    Updated the Java enum test to show how methods can be overriden for each instance field.

--- src/test/java/com/waterwagen/study/corejava/TestJavaEnums.java
+++ src/test/java/com/waterwagen/study/corejava/TestJavaEnums.java
@@ -14,21 +14,39 @@ public class TestJavaEnums
 		fruit = Fruit.APPLE;
 		assertEquals("Unexpected fruit price.", Double.valueOf(1.25), fruit.getPrice());
 		assertEquals("Unexpected fruit description.", "A red, sweet, round fruit.", fruit.getDescription());
-		
+		assertEquals("Unexpected string value. Each fruit should have overriden the stringValue() method.", 
+						"This is an apple string value.", fruit.stringValue());
+
 		fruit = Fruit.BANANA;
 		assertEquals("Unexpected fruit price.", Double.valueOf(0.99), fruit.getPrice());
 		assertEquals("Unexpected fruit description.", "A yellow, tube-shaped fruit.", fruit.getDescription());
+		assertEquals("Unexpected string value. Each fruit should have overriden the stringValue() method.", 
+						"This is a banana string value.", fruit.stringValue());		
 		
 		fruit = Fruit.ORANGE;
 		assertEquals("Unexpected fruit price.", Double.valueOf(1.45), fruit.getPrice());
 		assertEquals("Unexpected fruit description.", "An orange, sweet, juicy, round fruit.", fruit.getDescription());
+		assertEquals("Unexpected string value. Each fruit should have overriden the stringValue() method.", 
+						"This is an orange string value.", fruit.stringValue());
 	}
 	
 	private static enum Fruit
 	{
-		APPLE (1.25, "A red, sweet, round fruit."),
-		BANANA (0.99, "A yellow, tube-shaped fruit."),
-		ORANGE (1.45, "An orange, sweet, juicy, round fruit.");
+		APPLE (1.25, "A red, sweet, round fruit.")
+		{
+			@Override
+			protected String stringValue() { return "This is an apple string value."; }
+		},
+		BANANA (0.99, "A yellow, tube-shaped fruit.")
+		{
+			@Override
+			protected String stringValue() { return "This is a banana string value."; }
+		},
+		ORANGE (1.45, "An orange, sweet, juicy, round fruit.")
+		{
+			@Override
+			protected String stringValue() { return "This is an orange string value."; }
+		};
 		
 		private final Double mPrice;
 		private final String mDescription;
@@ -48,5 +66,10 @@ public class TestJavaEnums
 		{
 			return mDescription;
 		}		
+		
+		protected String stringValue()
+		{
+			return "This is the default string value.";
+		}
 	}
 }
