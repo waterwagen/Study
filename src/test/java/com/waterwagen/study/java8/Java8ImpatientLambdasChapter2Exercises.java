f1adce1f1820de7ecea213b89c19a0b09351bdf9
waterwagen <waterwagen@gmail.com>
2014-10-17 13:49:53 -0600

    Added J8FTRI chapter 2 exercise 2 solution.

--- src/test/java/com/waterwagen/study/java8/Java8ImpatientLambdasChapter2Exercises.java
+++ src/test/java/com/waterwagen/study/java8/Java8ImpatientLambdasChapter2Exercises.java
@@ -1,6 +1,7 @@
 package com.waterwagen.study.java8;
 
 import com.waterwagen.algorithms.evaluate.Stopwatch;
+import com.waterwagen.study.java8.Java8ImpatientLambdasChapter2ExercisesCompanion.*;
 import org.junit.Test;
 
 import java.util.LinkedList;
@@ -9,11 +10,13 @@ import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import com.waterwagen.study.java8.Java8ImpatientLambdasChapter2ExercisesCompanion.LargeWordCounter;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static com.waterwagen.study.java8.Java8ImpatientLambdasChapter2ExercisesCompanion.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Java8ImpatientLambdasChapter2Exercises {
 
@@ -42,6 +45,28 @@ public class Java8ImpatientLambdasChapter2Exercises {
     assertEquals(expectedLargeWordCount, actualLargeWordCount);
     System.out.println(String.format("expected large word count = %d, actual large word count = %d",
       expectedLargeWordCount, actualLargeWordCount));
+  }
+
+  @Test
+  public void exercise2() throws Exception {
+    List<String> words = getWords();
+
+    Predicate<? super String> wordLengthFunction = word -> word.length() > 4;
+    AtomicInteger filterCount = new AtomicInteger(0);
+    int maxNumberOfWords = 5;
+    List<String> collectedList = words.stream().sequential().filter(word -> {
+//      System.out.println(String.format("filtering %s", word));
+      filterCount.incrementAndGet();
+      return wordLengthFunction.test(word);
+    }).limit(maxNumberOfWords).collect(Collectors.toList());
+    System.out.println(
+      String.format("total number of long words is %d", words.stream().filter(wordLengthFunction).count()));
+    System.out.println(
+        String.format("filtered number of long words is %d", filterCount.get()));
+
+    assertEquals("Failed sanity check.", 5, collectedList.size());
+    assertTrue("Expected the stream to filter less than the total number of words as the number of results is limited.",
+      filterCount.get() < words.size());
   }
 
 }
